338f0cade8cc31aef3f6f03a60c3585bb822430d
xuyifei <937049042@qq.com>
2018-04-07 14:20:37 +0800

    ready

--- SAM3U2/firmware_common/drivers/leds.c
+++ SAM3U2/firmware_common/drivers/leds.c
@@ -237,6 +237,8 @@ Promises:
 void LedOff(LedNumberType eLED_)
 {
   u32 *pu32ClearAddress;
+  
+  pu32ClearAddress = 0;
 
   /* Configure set and clear addresses */
   if(Leds_asLedArray[eLED_].eActiveState == LED_ACTIVE_HIGH)
